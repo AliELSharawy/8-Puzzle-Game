0690efcbd57afe9ebf3dec529f1a397e92b90b7b
Habiba Osama <habebaosama14@gmail.com>
2022-10-23 18:11:57 +0200

    eg

--- 8-Puzzle-game/src/main/java/com/example/_8puzzlegame/Main.java
+++ 8-Puzzle-game/src/main/java/com/example/_8puzzlegame/Main.java
@@ -24,6 +24,45 @@ public class Main extends Application {
     //125340678
     //768243105
     //432650871
+       /*int[] puzzle = {
+                1, 2, 3,
+                8, 0, 4,
+                7, 6, 5
+        };*/
+
+    /* int[] puzzle = {
+                1, 2, 5,
+                3, 4, 0,
+               6, 7, 8
+        };
+//not solvable
+      /*  int[] puzzle = {
+                1, 2, 3,
+                4, 5, 6,
+                8,7 , 0
+        };*/
+
+    /* int[] puzzle = {
+             1, 0, 2,
+             7, 5, 4,
+             8, 6, 3
+     };*/
+        /*int[] puzzle = {
+                0, 1, 2,
+                3, 4, 5,
+                6, 7, 8
+        };*/
+    /*int[] puzzle = {
+            1, 4, 2,
+            6, 5, 8,
+            7, 3, 0
+    };*/  
+    /* int[] puzzle = {
+             1, 2, 3,
+             5, 6, 0,
+             7, 8, 4
+     };
+*/
     public static void begin(Stage stage) {
         try {
             FXMLLoader loader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
